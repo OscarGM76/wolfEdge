fc2203abf5616c3d2f08066d8a4b1abcfb16a8d9
Jose <joseortega@wolfsellers.com>
2025-06-13 12:42:57 -0600

    feat: update hero and social links

--- @wolfsellers/SocialLinks/socialLinks.js
+++ @wolfsellers/SocialLinks/socialLinks.js
@@ -1,18 +1,13 @@
 import { h } from '@dropins/tools/preact.js';
-import { useState } from '@dropins/tools/preact-hooks.js';
 import htm from 'htm';
 
 const html = htm.bind(h);
 
-const SocialLinks = () => {
-  const [first, setfirst] = useState(null);
-  return html`
-  <div>
-    <div>
-    linkedin
-    </div>
+const SocialLinks = ({ links }) => (
+  html`
+  <div class="socialLinsList">
+   ${links.map((link) => link)}
   </div>
-`;
-};
+`);
 
 export default SocialLinks;
--- blocks/hero-banner-content/hero-banner-content.js
+++ blocks/hero-banner-content/hero-banner-content.js
@@ -1,15 +1,21 @@
 import { decorateBlock } from '../../scripts/aem.js';
 
+
 export default function decorate(block) {
   try {
+    const heroBannerSecondColumn = document.querySelector('.heroBannerSecondColumn');
+    const heroBannerFirstColumnText = document.querySelector('.heroBannerFirstColumnText');
+    const parent = document.querySelector('.hero-banner-content-wrapper');
+    const picture = block.querySelector('div.hero-banner-content > div > div > picture');
+    picture.classList.add('hero-banner-content-picture');
+    heroBannerSecondColumn.append(picture);
     block.querySelectorAll('div.hero-banner-content > div > div').forEach((item) => {
-      const picture = item.querySelector('div > picture');
-      const blockName = picture ? 'hero-banner-content-picture' : 'hero-banner-content-text';
+      const blockName = 'hero-banner-content-text';
       item.classList.add(blockName);
       decorateBlock(item);
     });
-    const middleContentBanner = document.querySelector('#middleContentBanner');
-    middleContentBanner.append(block);
+    heroBannerFirstColumnText.append(block);
+    parent.remove();
   } catch (error) {
     console.log('error', error);
   }
