36e6af68bfb981668a02d9e8cbdb1ae98bd04b8f
Alejandro-Wolfsellers <alejandromaliachi@wolfsellers.com>
2025-07-02 13:50:15 -0600

    Background Block - V1.03
    - [code] - Refactor code

--- blocks/background/background.js
+++ blocks/background/background.js
@@ -1,5 +1,4 @@
 export default function decorate(block) {
-  let selector = null;
   let backgroundProperties = {
     'backgroundColor' : null,
     'backgroundImage' : null,
@@ -7,37 +6,25 @@ export default function decorate(block) {
     'backgroundRepeat' : null,
     'backgroundSize' : null,
   };
-  console.log("block", block);
-  const options = block.querySelectorAll(':scope > div');
-  console.log("options", options);
-  options.forEach((element) => {
-    console.log("element", element);
-    const index = element.querySelectorAll(':scope > div:first-child p')[0]?.innerHTML;
-    let value = element.querySelectorAll(':scope > div:last-child p')[0]?.innerHTML;
-    console.log("index", index);
-    console.log("value", value);
-    if (index === "Class") {
-      selector = document.querySelector("." + value);
-    }
-    if (index === "backgroundColor") {
-      backgroundProperties.backgroundColor = value;
-    }
-    if (index === "backgroundImage") {
-      value = element.querySelectorAll(':scope > div:last-child img')[0].getAttribute("src");
-      backgroundProperties.backgroundImage = `url(${value})`;
-    }
-    if (index === "backgroundPosition") {
-      backgroundProperties.backgroundPosition = value;
-    }
-    if (index === "backgroundRepeat") {
-      backgroundProperties.backgroundRepeat = value;
-    }
-    if (index === "backgroundSize") {
-      backgroundProperties.backgroundSize = value;
-    }
-    console.table(backgroundProperties);
-  });
-  Object.assign(selector.style, backgroundProperties);
-  selector.classList.add("bg-test");
-  block.remove();
+  const classList = block.classList;
+  classList.remove('background', 'block');
+  const selector = document.querySelector("."+block.classList[0]);
+  if(selector) {
+    const options = block.querySelectorAll(':scope > div');
+    options.forEach((element) => {
+      const index = element.querySelectorAll(':scope > div:first-child p')[0]?.innerHTML;
+      let value = element.querySelectorAll(':scope > div:last-child p')[0]?.innerHTML;
+      if (backgroundProperties.hasOwnProperty(index)) {
+        if (index === "backgroundImage") {
+          value = element.querySelectorAll(':scope > div:last-child img')[0].getAttribute("src");
+        }
+        backgroundProperties[index] = index === "backgroundImage" ? `url(${value})` : value
+      }
+    });
+    Object.assign(selector.style, backgroundProperties);
+  }
+  else {
+    console.warn("Background Component: Class selector not found!");
+  }
+  block.parentElement.remove();
 }
